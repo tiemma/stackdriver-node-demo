d0a681db59267d2bc8550894676af352f175414b
Bakare Emmanuel <bakareeemmanuel98@outlook.com>
2018-11-16 10:32:14 +0100

    Refactored logic and added error reporting tool for stackdriver

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var port = process.env.PORT || 3000;
-var io = require('socket.io')(http);
-var randomNumber = Math.floor(Math.random() * 255);
-var requestRandomNumber = null;
+const app = require('express')();
+const http = require('http').Server(app);
+const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  requestRandomNumber = Math.floor(Math.random() * 255)
+const randomNumber = Math.floor(Math.random() * 255);
+let requestRandomNumber = null;
+
+
+//Import local environment config
+require("dotenv").config();
+
+// Imports the Google Cloud client library
+const ErrorReporting = require('@google-cloud/error-reporting').ErrorReporting;
+
+// With ES6 style imports via TypeScript or Babel, the following
+// syntax can be used instead:
+// import {ErrorReporting} from '@google-cloud/error-reporting';
+
+// Instantiates a client
+const errors = new ErrorReporting();
+
+
+app.get('/', function (req, res, next) {
+  requestRandomNumber = Math.floor(Math.random() * 255);
   if (requestRandomNumber > 150) {
     //Let's purposefully break our app
-    res.send({ error: true, random: randomNumber }).status(500)
-    process.emit('SIGINT');
+    const status = Math.floor(Math.random() * (500 - 400)) + 400;
+    res.send({ error: true, random: randomNumber }).status(status);
+    next(new Error(`Error was caused by a random number greater than 150 with 
+    value ${requestRandomNumber} 
+    and status ${status}`));
+
   } else {
     res.send(`
     Random number for this pod is: + ${randomNumber}. 
     <br \>
     You env variable with property key is: ${process.env.KEY}
-    `);
+    `).status(200);
   }
 });
 
@@ -28,23 +47,14 @@ app.get('/logs', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+// Note that express error handling middleware should be attached after all
+// the other routes and use() calls. See the Express.js docs.
+app.use(errors.express);
+
+
 http.listen(port, function () {
   console.log('listening on *:' + port);
 });
 
-io.on('connection', function (socket) {
-  console.log(socket.id);
-
-  socket.emit('acknowledge', true);
-
-  // when socket disconnects, remove it from the list:
-  socket.on("disconnect", function () {
-    console.info(`Client gone [id=${socket.id}]`);
-  });
-});
-
-process.on('SIGINT', function () {
-  console.log("Caught interrupt signal, shutting down!");
-  io.emit('consume', `Error occured on pod ${randomNumber} due to random number ${requestRandomNumber}`);
-});
 
---
+++ run_test.sh
@@ -0,0 +1,4 @@
+for i in {1..10} 
+do 
+	curl localhost:3000;
+ done
